1032587b4f09153b47fc9b641385af7932d9e06b
jude okoroafor <judeokoroafor@gmail.com>
2022-12-20 22:47:39 +0100

    Create loading-screen.module.ts

---
+++ src/app/components/loading-screen/loading-screen.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingScreenComponent } from './loading-screen.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoadingScreenComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LoadingScreenModule { }
