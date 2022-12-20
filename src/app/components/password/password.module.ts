eba82f2502b7edcd8a27ae26a38a553ea3c7ef13
jude okoroafor <judeokoroafor@gmail.com>
2022-12-20 22:37:32 +0100

    password component

---
+++ src/app/components/password/password.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PasswordComponent } from './password.component';
+
+
+
+@NgModule({
+  declarations: [
+    PasswordComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PasswordModule { }
