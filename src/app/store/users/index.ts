2b8010c8d50c95bab2b01352ec3ab55f5642ad88
jude okoroafor <judeokoroafor@gmail.com>
2022-12-22 12:14:06 +0100

    created actions for store

---
+++ src/app/store/users/index.ts
@@ -0,0 +1,5 @@
+export * as UserActions from './users.actions';
+export * from './users.reducer';
+export * from './users.selectors';
+export * from './users.model';
+export * from './users.effects';
